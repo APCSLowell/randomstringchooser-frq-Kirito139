c04a14d2272b8ceecabb31cb3dff30f1a9fa3443
Lennier Mulder <lenniermulder@icloud.com>
2025-04-27 21:23:38 -0700

    First attempt

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,8 +1,21 @@
 import java.util.*;
-public class RandomStringChooser
-{
+public class RandomStringChooser {
   /* to be implemented in part (a) */
-  
-  //Heads up! 
-  //You will get a very confusing error message until you have working code in part b as well
+  private ArrayList<String> validWords = new ArrayList<String>();
+  public RandomStringChooser(String[] words) {
+      for (string word : words) {
+          validWords.add(word);
+      }
+  }
+
+  public String getNext() {
+      int rando = (int)(Math.random() * validWords.size());
+      if (validWords.size() == 0) return "NONE";
+      else String ans = validWords.get(rando);
+      validWords.remove(rando);
+      return ans;
+  }
+  // Heads up! 
+  // You will get a very confusing error message until you have working code in
+  // part b as well
 }
